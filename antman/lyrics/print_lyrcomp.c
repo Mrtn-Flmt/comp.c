b3e123232ba0efd31f3c07fb8d117103ea27e4f6
thomas <thomas@localhost.localdomain>
2021-01-23 02:37:34 +0100

    [*] .lyr compression : putting spaces between numbers

--- antman/lyrics/print_lyrcomp.c
+++ antman/lyrics/print_lyrcomp.c
@@ -14,7 +14,7 @@ void print_lyrcomp(char **cont, char **ind)
     for (int i_cont = 0; cont[i_cont] != NULL; i_cont++) {
         i = 1;
         for (int i_ind = 0; str_compare(ind[i_ind], cont[i_cont]) == 0; i++, i_ind++);
-        my_printf("%i", i);
+        my_printf("%i ", i);
     }
     my_printf("\n");
 }
